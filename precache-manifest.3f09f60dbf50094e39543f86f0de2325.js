4b646f208abf55477fd38dd9ab6829ce3d57601c
Me <russzkij@yahoo.com>
2021-11-02 23:47:36 +0100

    Updates

---
+++ precache-manifest.3f09f60dbf50094e39543f86f0de2325.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fba8f4c8731f14ccacb8c213bcbd68d",
+    "url": "/readexcel/index.html"
+  },
+  {
+    "revision": "378f550026bae9c09ac0",
+    "url": "/readexcel/static/css/main.8505c57d.chunk.css"
+  },
+  {
+    "revision": "5345b3ac73893c05c98d",
+    "url": "/readexcel/static/js/2.7c51162b.chunk.js"
+  },
+  {
+    "revision": "217bf2f79f3ef6c823e9a508ba02f5ac",
+    "url": "/readexcel/static/js/2.7c51162b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "378f550026bae9c09ac0",
+    "url": "/readexcel/static/js/main.676726af.chunk.js"
+  },
+  {
+    "revision": "8796e450056e51e050aa",
+    "url": "/readexcel/static/js/runtime-main.e7c25368.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/readexcel/static/media/logo.5d5d9eef.svg"
+  }
+]);
